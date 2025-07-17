b355385f2eba2b25789672321aadd96802f0a8a9
Justsmtp <smtpwtf@gmail.com>
2025-07-17 18:20:14 +0100

    Add DRF router and include chat routes under /api path

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,8 +1,9 @@
+# messaging_app/chats/urls.py
 from django.urls import path, include
-from rest_framework import routers 
+from rest_framework import routers
 from .views import ConversationViewSet, MessageViewSet
 
-router = routers.DefaultRouter()  
+router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
 
